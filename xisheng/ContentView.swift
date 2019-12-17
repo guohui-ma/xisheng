0f3cfb8e1b2cb0e16857142fd0c24f9f76704843
maguohui <godwin_ma@163.com>
2019-12-17 17:17:23 +0800

    u

---
+++ xisheng/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  xisheng
+//
+//  Created by gavin on 2019/12/17.
+//  Copyright © 2019 chuhanju. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
